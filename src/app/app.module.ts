c66c9c1307de0d6a3eca5a416682341753939683
Madhavraob <madhavrao039@gmail.com>
2021-04-10 22:23:32 +0530

    jira grid layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RecruitersComponent } from './recruiters/recruiters.component';
+import { InterviewsComponent } from './interviews/interviews.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecruitersComponent,
+    InterviewsComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
